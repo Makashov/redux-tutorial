231fb2a63efbda3f859b6676c325d0a3ea19ed79
Makashov <makashov92@gmail.com>
2020-01-20 14:00:59 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: [
+        { title: "Test 1", id: 1 },
+        { title: "Test 2", id: 2 },
+      ]
+    };
+  }
+
+  render() {
+    const { articles } = this.state;
+
+    return (
+        <ul>
+          { articles.map(el => <li key={el.id}>{el.title}</li>) }
+        </ul>
+    );
+  }
 }
 
 export default App;
---
+++ src/js/reducers/index.js
@@ -0,0 +1,18 @@
+import { ADD_ARTICLE } from "../constants/actions-types";
+
+const initialState = {
+    articles: []
+};
+
+function rootReducer(state = initialState, action) {
+    if (action.type === ADD_ARTICLE) {
+        return {
+            ...state,
+            articles: state.articles.concat(action.payload)
+        };
+    }
+
+    return state;
+}
+
+export default rootReducer;
